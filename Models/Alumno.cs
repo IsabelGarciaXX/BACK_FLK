f36bb52ffda373724ac006eb4c17cc89041a395e
IsabelGarciaXX <isabelcielogarciahuapaya@gmail.com>
2024-05-30 16:32:57 -0500

    Cambios controladores v.2

--- Models/Alumno.cs
+++ Models/Alumno.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace BACK_FLK.Models;
 
@@ -17,7 +18,10 @@ public partial class Alumno
 
     public int? FkTipoDocumentoIdentidad { get; set; }
 
+    [JsonIgnore]
     public virtual ICollection<AlumnoXcurso> AlumnoXcursos { get; set; } = new List<AlumnoXcurso>();
 
+
+    [JsonIgnore]
     public virtual TipoDocumentoIdentidad? FkTipoDocumentoIdentidadNavigation { get; set; }
 }
