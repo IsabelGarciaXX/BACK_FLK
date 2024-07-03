6e58522115316e2514a2ee375140c151617a000c
adrianH0498 <202011420@urp.edu.pe>
2024-07-03 17:47:09 -0500

    Feat: Apis para CUS visualizar agenda general, trae Todas las inspecciones con los nombres de los inspectores y certificadores

---
+++ Controllers/AgendaGeneralController.cs
@@ -0,0 +1,59 @@
+﻿using BACK_FLK.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace BACK_FLK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgendaGeneralController : ControllerBase
+    {
+        private readonly BdFlkContext _context;
+
+        public AgendaGeneralController(BdFlkContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("InspeccionesConNombres")]
+        public async Task<ActionResult<IEnumerable<object>>> GetInspeccionesConNombres()
+        {
+            var inspecciones = await _context.Inspecciones
+                .Include(i => i.FkCertificadorAsignadoNavigation)
+                    .ThenInclude(c => c.FkUsuarioNavigation)
+                        .ThenInclude(u => u.Personals)
+                .Include(i => i.FkInspectoresAsignadosNavigation)
+                    .ThenInclude(a => a.FkInpectoresDisponiblesNavigation)
+                        .ThenInclude(d => d.FkUsuarioNavigation)
+                            .ThenInclude(u => u.Personals)
+                .Include(i => i.FkTipoInspeccionNavigation)
+                .Include(i => i.FkVehiculoNavigation)
+                .Include(i => i.FkEmpresasNavigation)
+                .Select(i => new
+                {
+                    i.FechaHoraInicio,
+                    i.FechaHoraFinalizacion,
+                    CertificadorNombre = i.FkCertificadorAsignadoNavigation.FkUsuarioNavigation.Personals.FirstOrDefault().Nombre,
+                    InspectoresList = i.FkInspectoresAsignadosNavigation != null
+                                      ? new List<object> {
+                                          new {
+                                              InspectorNombre = i.FkInspectoresAsignadosNavigation.FkInpectoresDisponiblesNavigation.FkUsuarioNavigation.Personals.FirstOrDefault().Nombre
+                                          }
+                                        }
+                                      : new List<object>(),
+                    i.FkTipoInspeccionNavigation.Titulo,
+                    i.FkVehiculoNavigation.Marca,
+                    i.Ubicacion,
+                    EmpresaRuc = i.FkEmpresasNavigation.Ruc,
+                    EmpresaRazonSocial = i.FkEmpresasNavigation.RazonSocial,
+                })
+                .ToListAsync();
+
+            return Ok(inspecciones);
+        }
+    }
+}
---
+++ Controllers/RecuperacionContraseñaController.cs
@@ -0,0 +1,19 @@
+﻿using BACK_FLK.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACK_FLK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecuperacionContraseñaController : ControllerBase
+    {
+        private readonly BdFlkContext _context;
+
+        public AgendaDeUsuarioController(BdFlkContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+    }
+}
